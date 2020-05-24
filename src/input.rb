2f7fbbeb07a9b5580210d675d8a8b5cbb6329a97
Dominik Nannen <brickmaster25@gmail.com>
2020-05-24 15:20:20 +0200

    First implementation

--- src/input.rb
+++ src/input.rb
@@ -1,3 +1,17 @@
 class Net
-  attr_accessor :
+  attr_accessor :places, :transitions, :flow, :weight, :marks
+
+  def initialize(one, two, three, four, five)
+    self.places = one
+    self.transitions = two
+    self.flow = three
+    self.weight = four
+    self.marks = five
+  end
+
+
 end
+
+
+net = Net.new("", "", "", "", "")
+
