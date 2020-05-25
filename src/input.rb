9e1c925ed45c449064851a4fee816195ec020fc7
dnannen <dominik.nannen@uni-oldenburg.de>
2020-05-25 20:03:50 +0200

    Implemented the input algorithm

--- src/input.rb
+++ src/input.rb
@@ -9,7 +9,8 @@ flowstring = gets.chomp
 # Placing the net compartments in arrays
 places = placestring.split(',')
 transitions = transitionstring.split(',')
-flow = flowstring.split(';',',')
+flow = flowstring.split(';')
+
 
 
 # Making the graphViz-File
@@ -19,17 +20,21 @@ graph = File.new("graph.gv","w")
 graph.puts("digraph petrinet{")
 graph.puts("node[shape=circle];")
 
+# Adding places to graphViz-File
 places.each do |place|
   graph.print '"'
   graph.print place
   graph.puts '";'
 end
+
+# Adding transitions to graphViz-File
 transitions.each do |transition|
   graph.print '"'
   graph.print transition
   graph.puts '" [shape=box];'
 end
 
+# Adding the flow relation to graphViz-File
 
 graph.puts flow
 graph.close
