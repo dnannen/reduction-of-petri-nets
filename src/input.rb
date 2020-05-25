24a07117996731cad0eea2af5c379628bc160365
dnannen <dominik.nannen@uni-oldenburg.de>
2020-05-25 19:59:36 +0200

    SETUP 1

--- src/input.rb
+++ src/input.rb
@@ -1,19 +1,35 @@
-puts("String eingeben!")
-pm = gets.chomp
+require 'pp'
+
+# Eingabe des Netzes
+puts "Bitte Stellen, Transitionen und Übergänge angeben!"
+placestring = gets.chomp
+transitionstring = gets.chomp
+flowstring = gets.chomp
+
+# Placing the net compartments in arrays
+places = placestring.split(',')
+transitions = transitionstring.split(',')
+flow = flowstring.split(';',',')
 
 
-places = gets.chomp
-allPlaces = []
-transitions = gets.chomp
-flow = gets.chomp
-
+# Making the graphViz-File
 graph = File.new("graph.gv","w")
-graph.puts(places)
-graph.puts(transitions)
-graph.puts(flow)
+
+# Hardcoded input for making the file work
+graph.puts("digraph petrinet{")
+graph.puts("node[shape=circle];")
+
+places.each do |place|
+  graph.print '"'
+  graph.print place
+  graph.puts '";'
+end
+transitions.each do |transition|
+  graph.print '"'
+  graph.print transition
+  graph.puts '" [shape=box];'
+end
+
+
+graph.puts flow
 graph.close
-
-puts places
-puts transitions
-puts flow
-
