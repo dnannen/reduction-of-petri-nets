08d88f1120b081ca070c3d45910180a878b33bb7
Dominik Nannen <brickmaster25@gmail.com>
2020-06-04 01:29:56 +0200

    Mehr updates.

---
+++ src/Laufstellen.rb
@@ -0,0 +1,2 @@
+# ist s im vorbereich, so ist s auch im nachbereich
+# jede Transition kann schalten, die im nachbereich liegt
--- src/paralleleKnoten.rb
+++ src/paralleleKnoten.rb
@@ -1,46 +1,40 @@
 places = %w[s1 s2]
 marks = [0, 1]
 transitions = %w[t1 t2 t3]
-flow = %w[t1 s1 f t1 s2 f t2 s1 f t2 s2 f s1 t3 f s2 t3 f]
+flow = %w[t1 s1 t1 s2 t2 s1 t2 s2 s1 t3 s2 t3]
 
-# Arrays für den Vor- und Nachbereich zweier Transitionen
-pre = []
-post = []
 index = 0
-places.each do |s|
-  flow.each do |f|
-    # Element in der Flussrelation ist eine Stelle
-    if s == f
-      # Der Bogen geht von s aus
-      if (flow[index - 1] = nil || flow[index - 1] = 'f')
-        # Sofern das Element eine Transition ist, füge sie zum Nachbereich hinzu
-        # Durchlauf
-        if run == 0
-          posts1.append(flow[index + 1]) if transitions.include? flow[index + 1]
-        elsif run == 1
-          posts2.append(flow[index + 1]) if transitions.include? flow[index + 1]
-        end
-        # Führt der Bogen allerdings zu s, ...
-      elsif (flow[index + 1] = nil || flow[index + 1] = 'f')
-        # Füge die vorherige Transition in den Vorbereich ein,
-        # sofern es eine Transition ist
-        if run == 0
-          pres1.append(flow[index - 1]) if transitions.include? flow[index - 1]
-        elsif run == 1
-          pres2.append(flow[index - 1]) if transitions.include? flow[index - 1]
-        end
-      end
-    end
+from = []
+to = []
+flow.each do |f|
+  if(index.even?)
+    from.append(f)
+  else
+    to.append(f)
   end
-  # Erhöhen der Laufnummer
-  if run == 0
-    run += 1
-  elsif run == 1
-    run -= 1
-  end
+  index += 1
 end
 
-  puts pres1
-  puts pres2
-  puts posts1
-  puts posts2
+# Vor- und Nachbereiche der Stellen füllen
+pre = post = []
+index = 0
+
+# Fülle Nachbereich
+from.each do |f|
+  if places.include?(f)
+    post.append(to[index])
+  end
+end
+post.append('f')
+
+# Fülle Vorbereich
+to.each do |o|
+  if places.include?(o)
+    pre.append(from[index])
+  end
+end
+pre.append('f')
+
+puts pre
+puts 'uwu'
+puts post
