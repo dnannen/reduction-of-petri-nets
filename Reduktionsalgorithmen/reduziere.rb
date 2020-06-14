2598e7b3c2644cb7132903dfe7378f8eb036a63c
Dominik Nannen <brickmaster25@gmail.com>
2020-06-14 13:55:23 +0200

    lauf_stellen.rb entfernt nun Laufstellen.
    schlingen.rb angefangen, Reduktion folgt.
    
    Rest im pdf der Arbeit.

---
+++ Reduktionsalgorithmen/reduziere.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+require File.join(Dir.pwd, 'petri_netz.rb')
+
--- Reduktionsalgorithmen/schlingen.rb
+++ Reduktionsalgorithmen/schlingen.rb
@@ -1,1 +1,18 @@
+# frozen_string_literal: true
 
+require File.join(Dir.pwd, 'petri_netz.rb')
+
+schlinge = PetriNetz.new('s1:t1,t2;s2:t1,t2;;t1:s1,s2;t2:;;', '0,0')
+
+# Prüfe jede Transition
+schlinge.transitionen.each do |t|
+  schlinge.hin.each do |_h|
+    # Vorbereich identisch mit Nachbereich
+    next unless schlinge.hin.join(', ').split(', ')[schlinge.transitionen.index(t)] ==
+                schlinge.her.join(', ').split(', ')[schlinge.transitionen.index(t)]
+    # TODO
+  end
+end
+
+schlinge.testnetz
+schlinge.gv
