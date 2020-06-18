32252acfe5383ca3b9513b7d0e97aecdd6e1ba46
dnannen <dominik.nannen@uni-oldenburg.de>
2020-06-18 15:52:11 +0200

    Speichercommit.

--- Reduktionsalgorithmen/vor_und_nachtransitionen.rb
+++ Reduktionsalgorithmen/vor_und_nachtransitionen.rb
@@ -6,35 +6,37 @@ require File.join(Dir.pwd, 'petri_netz.rb')
 vorundnach = PetriNetz.new('s1:t1,t2;s2:t4;s3:;s4:t3;;t1:s3;t2:s4;t3:s4,s4;t4:s1,s3;;', '0,1,0,0')
 
 # Regel 5
+# Erstelle ein Array mit den Stellen, die alle Voraussetzungen erfüllen.
+kandidaten = []
+
 # Prüfe alle Stellen
-# Erstelle ein Array mit den Stellen, die alle Voraussetzungen erfüllen
-# Zu Beginn sind alle Stellen Kandidaten
-kandidaten = vorundnach.stellen
 vorundnach.stellen.each do |s|
 
-  # Prüfe zuerst, ob Vor- oder Nachbereich leer sind
-  kandidaten.delete(s) if vorundnach.vorbereich(s) == [] || vorundnach.fluss[s].nil?
+  # Vor- oder Nachbereich dürfen nicht leer sein
+  next if vorundnach.vorbereich(s) == [] || vorundnach.fluss[s].nil?
 
-  # Es darf keine Schleife an s anliegen
-  # Prüfe für jede Transition im Nachbereich
-  vorundnach.fluss[s].each do |n|
-    # Kommt der Vorbereich im Nachbereich vor?
-    next if vorundnach.vorbereich(s).include? n
+  # Es gibt keine Schlinge an s
+  next if vorundnach.vorbereich(s).include? vorundnach.fluss[s].join(', ')
 
-    # Der Nachbereich vom Nachbereich ist nicht leer
-    next if vorundnach.fluss[n].nil?
+  # Füge alle bisher zutreffenden Stellen zu den Kandidaten hinzu
+  kandidaten.append(s)
+end
 
-    # Füge s den Kandidaten hinzu, sofern es noch kein Kandidat ist
-    kandidaten.append(s) unless kandidaten.include?(s)
+# Sortiere die Kandidaten weiter aus
+kandidaten.each do |k|
+  # Prüfe alle Nachbereichstransitionen von k
+  vorundnach.fluss[k].each do |nt|
+
+    # Entferne k aus den Kandidaten, sofern die Nachbereichstransitionen
+    # noch andere Stellen außer k im Vorbereich haben
+    kandidaten.delete(k) if vorundnach.vorbereich(nt).join(', ') != k
+
+
   end
-
-  # Der Vorbereich vom Nachbereich beläuft sich nur auf s
-
 end
 
 p kandidaten
 
-vorundnach.deisoliere
-
+#vorundnach.deisoliere
 vorundnach.testnetz
 vorundnach.gv
