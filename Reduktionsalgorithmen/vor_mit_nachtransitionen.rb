f6bf34c3d76579f867595c1596cee5e04352d50c
dnannen <dominik.nannen@uni-oldenburg.de>
2020-06-23 17:46:13 +0200

    vor_mit_nachtransitionen.rb fertiggestellt. Fehler bei der Überprüfung der Voraussetzungen führt zu falschem Ergebnis.

--- Reduktionsalgorithmen/vor_mit_nachtransitionen.rb
+++ Reduktionsalgorithmen/vor_mit_nachtransitionen.rb
@@ -4,6 +4,7 @@ require File.join(Dir.pwd, 'petri_netz.rb')
 
 # Testobjekt für diesen Reduktionsschritt
 vormitnach = PetriNetz.new('s1:t1,t2;s2:t0;s3:t2;s4:t4;s5:t5;;t0:s1;t1:s4;t2:s5;t4:s2;t5:s2,s3;;', '0,1,0,0,0')
+index = 1
 
 # Regel 6:
 # Im Vorbereich von s befindet sich genau eine Transition t.
@@ -14,7 +15,6 @@ vormitnach = PetriNetz.new('s1:t1,t2;s2:t0;s3:t2;s4:t4;s5:t5;;t0:s1;t1:s4;t2:s5;
 
 # Prüfe alle Stellen
 vormitnach.stellen.each do |s|
-
   # Es gibt genau eine Transition im Vorbereich von s
   next unless vormitnach.vorbereich(s).length == 1
 
@@ -29,41 +29,64 @@ vormitnach.stellen.each do |s|
           vormitnach.fluss[s].include?(vormitnach.vorbereich(s).join(', '))
 
   # Alle Vorbereichsstellen von t haben nur t im Nachbereich
-  fuenf = true
-  vormitnach.vorbereich(s).join(', ').split(', ').each do |v|
-    fuenf if s == vormitnach.fluss[v].join(', ')
+  fuenf = false
+  vormitnach.vorbereich(vormitnach.vorbereich(s).join(', ')).each do |f|
+    fuenf = vormitnach.fluss.values_at(f).join(', ') == vormitnach.vorbereich(s).join(', ')
   end
   next unless fuenf
 
+  p s
+
   # Alle Kanten die s enthalten, haben die selbe Vielfachheit
-  sechs = false
-  p vormitnach.fluss[s].all? { |n| vormitnach.fluss[s].count(vormitnach.fluss[n].last) }
-  p vormitnach.vorbereich(s).all? { |v| vormitnach.vorbereich(s).count(vormitnach.vorbereich(s).last) }
-
-
-  #
-  vielfachheit = 0
-  vormitnach.fluss.each do |f|
-
-  end
-
+  next unless vormitnach.fluss[s].all? do |n|
+    vormitnach.fluss[s].count(n) ==
+    vormitnach.fluss[s].count(vormitnach.fluss[s].last)
+  end &&
+              vormitnach.vorbereich(s).all? do |v|
+                vormitnach.vorbereich(s).count(v) ==
+                vormitnach.vorbereich(s).count(vormitnach.vorbereich(s).last)
+              end
 
   # Reduziere die Stelle s
   # Für jede Transition aus dem Nachbereich von s
   # Definiere benötigte Bereiche für später
   vorbereich = []
   nachbereich = []
-  vormitnach.fluss[s].each_with_index do |n, i|
-    # Führe eine neue Transition t. ein
-    vormitnach.transitionen.append('t.' + i.to_s)
+  vormitnach.fluss[s].each do |n|
+    # Führe eine neue Transition t.i ein
+    vormitnach.transitionen.append('t.' + index.to_s)
 
-    # Der Vorbereich der neuen besteht aus den Vorbereichen des Vorbereichs von s und dem Nachbereich von s
+    # Der Vorbereich der neuen Transition besteht aus den Vorbereichen des Vorbereichs von s und dem Nachbereich von s
     # Vorbereich von s
-    vorbereich.append vormitnach.vorbereich(s)
+
+    vorbereich.append vormitnach.vorbereich(vormitnach.vorbereich(s).join(', '))
     # Vorbereiche des Nachbereichs von s
     vorbereich.append(vormitnach.vorbereich(n))
+    # Der Nachbereich besteht aus dem Nachbereich der Nachbereiche von s
+    nachbereich.append(vormitnach.fluss[n])
 
+    # Füge die neuen Transitionen mitsamt definiertem Vor- und Nachbereich ins Netz ein
+    vorbereich.join(', ').split(', ').each do |nv|
+      vormitnach.fluss[nv] = vormitnach.fluss[nv] + ['t.' + index.to_s]
+      vormitnach.fluss[nv] = vormitnach.fluss[nv] - [s]
+    end
+    nachbereich.join(', ').split(', ').each do |nn|
+      vormitnach.fluss.store(vormitnach.transitionen.last, [nn])
+    end
+
+    # Streiche die bereits bearbeitete Nachbereichstransition
+    vormitnach.reduziere_knoten(n)
+
+    # Aktualisiere die Variablen für den nächsten Durchlauf
+    vorbereich.clear
+    nachbereich.clear
+    index += 1
   end
+  # Streiche die Vorbereichstransition von s
+  vormitnach.reduziere_knoten(vormitnach.vorbereich(s).join(', '))
+
+  # Streiche den Knoten s
+  vormitnach.reduziere_knoten(s)
 end
 
 vormitnach.testnetz
